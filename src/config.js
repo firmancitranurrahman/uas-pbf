e5970faa4ed3f5cf4fe1fd0a28bf55992d5841da
firmancitranurrahman <firmancitranurrahman@gmail.com>
2020-05-23 09:19:36 +0700

    finish

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+const config = {
+  apiKey: "AIzaSyBbj-nfXOpnOM2LNmmggoOMMdvvWcl45kQ",
+  authDomain: "uas-pbf-baf83.firebaseapp.com",
+  databaseURL: "https://uas-pbf-baf83.firebaseio.com",
+  projectId: "uas-pbf-baf83",
+  storageBucket: "uas-pbf-baf83.appspot.com",
+  messagingSenderId: "14687059117",
+  appId: "1:14687059117:web:90606a80945c88ab261a24"
+};
+export default config;
